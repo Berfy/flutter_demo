1a1854b408b0e4f20daf467cc77fff8e05cc3493
BerfyYue <48935452+BerfyYue@users.noreply.github.com>
2019-07-05 13:36:51 +0800

    preview

---
+++ lib/model/post.dart
@@ -0,0 +1,59 @@
+class Post {
+  final String title;
+  final String author;
+  final String imageUrl;
+
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+}
+
+final List<Post> posts = [
+  Post(
+    title: "111111",
+    author: 'Hanmo',
+    imageUrl: 'http://pic23.nipic.com/20120810/5736735_162511503170_2.jpg',
+  ),
+  Post(
+    title: "222222",
+    author: 'mixiu',
+    imageUrl: 'http://pic39.nipic.com/20140321/18063302_210604412116_2.jpg',
+  ),
+  Post(
+    title: "333333",
+    author: 'Berfy',
+    imageUrl: 'http://pic37.nipic.com/20140113/8800276_184927469000_2.png',
+  ),
+  Post(
+    title: "111111",
+    author: 'Hanmo',
+    imageUrl: 'http://pic23.nipic.com/20120810/5736735_162511503170_2.jpg',
+  ),
+  Post(
+    title: "222222",
+    author: 'mixiu',
+    imageUrl: 'http://pic39.nipic.com/20140321/18063302_210604412116_2.jpg',
+  ),
+  Post(
+    title: "333333",
+    author: 'Berfy',
+    imageUrl: 'http://pic37.nipic.com/20140113/8800276_184927469000_2.png',
+  ),
+  Post(
+    title: "111111",
+    author: 'Hanmo',
+    imageUrl: 'http://pic23.nipic.com/20120810/5736735_162511503170_2.jpg',
+  ),
+  Post(
+    title: "222222",
+    author: 'mixiu',
+    imageUrl: 'http://pic39.nipic.com/20140321/18063302_210604412116_2.jpg',
+  ),
+  Post(
+    title: "333333",
+    author: 'Berfy',
+    imageUrl: 'http://pic37.nipic.com/20140113/8800276_184927469000_2.png',
+  ),
+];
